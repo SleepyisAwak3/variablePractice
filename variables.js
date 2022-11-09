6ae815c846919a7266e0543a064535edba86005e
Noah Byers <noahxbyers@gmail.com>
2022-11-09 07:40:55 -0800

    Add more variables and redefinitions

--- variables.js
+++ variables.js
@@ -1,6 +1,35 @@
 var email = "noahxbyers@gmail.com"
-  console.log(email)
-var votingAge = 18
-  console.log(votingAge)
-var playerOnlineStatus = true
-  console.log(playerOnlineStatus)
+  console.log(email);
+
+  email = "noahxbyers@hotmail.com"
+  console.log(email);
+
+  var userName = "SleepyisAwake"
+  console.log(userName);
+
+userName = "Sleepywookie7"
+  console.log(userName);
+
+  var votingAge = 18
+  console.log(votingAge);
+
+  votingAge = 21
+  console.log(votingAge);
+
+  var numberOfEggs = 32
+ console.log(numberOfEggs);
+ 
+numberOfEggs = 441
+  console.log(numberOfEggs);
+
+  var playerOnlineStatus = true
+  console.log(playerOnlineStatus);
+
+  playerOnlineStatus = false
+  console.log(playerOnlineStatus);
+
+  var gameBought = true
+  console.log(gameBought);
+
+  gameBought = false
+  console.log(gameBought);
